5af37596094bf6d317a3bcb40ee0de93d034d44b
adamthies17 <adamthies17@gmail.com>
2025-04-16 15:14:09 +0000

    done with lab

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 #Name:
 #Date:
 #Assignment:
+import matplotlib.pyplot as plt
+import consumer_price_index
+report = consumer_price_index.get_report()
+num_items = len(report)
+prices = []
+years = []
+for spot in range(num_items):
+    year = report[spot]["Year"]
+    meat_price = report[spot]["Data"]["Meat"]["Beef"]
+    
+    
+    prices.append(meat_price)
+    years.append(year)
+
+
+plt.plot(years, prices, 'ro')
+plt.ylabel('Meat Price')
+plt.xlabel("Year")
+plt.savefig("Output")
